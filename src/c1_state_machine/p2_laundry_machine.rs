75452a01e5053844ae6a9db080a2bc8f7018d2bc
kolasniwash <kolasniwash@gmail.com>
2024-04-20 18:59:07 +0100

    c1-p2 washing machine solution

--- src/c1_state_machine/p2_laundry_machine.rs
+++ src/c1_state_machine/p2_laundry_machine.rs
@@ -40,7 +40,43 @@ impl StateMachine for ClothesMachine {
 	type Transition = ClothesAction;
 
 	fn next_state(starting_state: &ClothesState, t: &ClothesAction) -> ClothesState {
-		todo!("Exercise 3")
+
+		match starting_state {
+			ClothesState::Tattered => ClothesState::Tattered,
+			ClothesState::Clean(life) => {
+				if 0 == life - 1 {
+					ClothesState::Tattered
+				} else {
+					match t {
+						ClothesAction::Wear => {ClothesState::Dirty(life - 1)}
+						ClothesAction::Wash => {ClothesState::Wet(life - 1)}
+						ClothesAction::Dry => { ClothesState::Clean(life - 1)}
+					}
+				}
+			},
+			ClothesState::Dirty(life) => {
+				if 0 == life - 1 {
+					ClothesState::Tattered
+				} else {
+					match t {
+						ClothesAction::Wear => {ClothesState::Dirty(life - 1)}
+						ClothesAction::Wash => {ClothesState::Wet(life - 1)}
+						ClothesAction::Dry => { ClothesState::Dirty(life - 1)}
+					}
+				}
+			},
+			ClothesState::Wet(life) => {
+				if 0 == life - 1 {
+					ClothesState::Tattered
+				} else {
+					match t {
+						ClothesAction::Wear => {ClothesState::Wet(life - 1)}
+						ClothesAction::Wash => {ClothesState::Wet(life - 1)}
+						ClothesAction::Dry => { ClothesState::Clean(life - 1)}
+					}
+				}
+			}
+		}
 	}
 }
 
